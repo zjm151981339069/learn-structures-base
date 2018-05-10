87f4dd573ced91c59ef5667713b55b41d88b0a0d
Administrator <1141266205@qq.com>
2018-05-10 17:20:36 +0800

    2

--- learn-structures-base/src/main/java/com/zjm/learn/structures/learn/structures/base/d1_array/S2FastSort.java
+++ learn-structures-base/src/main/java/com/zjm/learn/structures/learn/structures/base/d1_array/S2FastSort.java
@@ -1,5 +1,9 @@
 package com.zjm.learn.structures.learn.structures.base.d1_array;
-
+/**
+ * 快速排序，其基本思路是： 选择一个基准数据，使得一部分数据小于基准数据，另一部分数据大于基准数据，通过递归的方法完成排序
+ * @author Administrator
+ *
+ */
 public class S2FastSort {
 
 }
---
+++ learn-structures-base/src/main/java/com/zjm/learn/structures/learn/structures/base/d2_list/DoubleLink.java
@@ -0,0 +1,38 @@
+package com.zjm.learn.structures.learn.structures.base.d2_list;
+
+public class DoubleLink<T> {
+	private DNode<T> mHead; 
+	private int mCount = 0;
+	
+	
+	// 该类承载前后数据指针以及对象本身的数据内容
+	private class DNode<T>{
+		public DNode prev;
+		public DNode next;
+		public T value;
+		public DNode(DNode prev, DNode next, T value) {
+			this.prev = prev;
+			this.next = next;
+			this.value = value;
+		}
+	}
+	
+	//构造函数
+	public DoubleLink() {
+		mHead = new DNode<T>(null,null,null);
+		mHead.prev= mHead.next = mHead;
+		mCount = 0;
+	}
+	
+	
+	// 返回节点数据长度
+	public int size() {
+		return mCount;
+	}
+	// 返回链表是否为空
+	
+	public boolean isEmpty() {
+		return mCount == 0;
+	}
+	
+}
